76f541d7de57304cafc4d98324e47c398c0c5915
Maximilian Kenfenheuer <maximilian.kenfenheuer@ksol.it>
2022-06-06 16:08:50 +0200

    Initial Commit

---
+++ NeptuneController.cs
@@ -0,0 +1,117 @@
+﻿using hidapi;
+using neptune_hidapi.net.Hid;
+using neptune_hidapi.net.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neptune_hidapi.net
+{
+    public class NeptuneController
+    {
+        private HidDevice _hidDevice;
+        private ushort _vid = 0x28de, _pid = 0x1205;
+        private Task _configureTask;
+        private bool _active = false;
+
+        public bool LizardModeEnabled { get; set; }
+        public string SerialNumber { get; private set; }
+        public event EventHandler<NeptuneControllerInputEventArgs> OnControllerInputReceived;
+
+        public NeptuneController()
+        {
+            _hidDevice = new HidDevice(_vid, _pid, 64);
+            _hidDevice.OnInputReceived += OnInputReceived;
+        }
+
+        private void OnInputReceived(object sender, HidDeviceInputReceivedEventArgs e)
+        {
+            SDCInput input = e.Buffer.ToStructure<SDCInput>();
+            NeptuneControllerInputState state = new NeptuneControllerInputState(input);
+            OnControllerInputReceived?.Invoke(this, new NeptuneControllerInputEventArgs(state));
+        }
+
+        private async Task<bool> SetLizardMode(bool enabled)
+        {
+            try
+            {
+                if (!enabled)
+                {
+                    //Disable mouse emulation
+                    byte[] data = new byte[] { 0x87, 0x03, 0x08, 0x07 };
+                    await _hidDevice.RequestFeatureReportAsync(data);
+
+                    //Disable keyboard/mouse button emulation
+                    data = new byte[] { 0x81, 0x00 };
+                    await _hidDevice.RequestFeatureReportAsync(data);
+                }
+                else
+                {
+                    //Enable keyboard/mouse button emulation
+                    byte[] data = new byte[] { 0x85, 0x00 };
+                    await _hidDevice.RequestFeatureReportAsync(data);
+
+                    //Enable mouse emulation
+                    data[0] = 0x8e;
+                    await _hidDevice.RequestFeatureReportAsync(data);
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private async Task ConfigureLoop()
+        {
+            while(_active)
+            {
+                await SetLizardMode(LizardModeEnabled);
+                await Task.Delay(250);
+            }
+        }
+
+        public async Task<string> ReadSerialNumberAsync()
+        {
+            byte[] request = new byte[] { 0xAE, 0x15, 0x01 };
+            byte[] response = await _hidDevice.RequestFeatureReportAsync(request);
+            byte[] serial = new byte[response.Length - 5];
+            Array.Copy(response, 4, serial, 0, serial.Length);
+
+            return Encoding.ASCII.GetString(serial).TrimEnd((Char)0);
+        }
+        public string ReadSerialNumber()
+        {
+            byte[] request = new byte[] { 0xAE, 0x15, 0x01 };
+            byte[] response = _hidDevice.RequestFeatureReport(request);
+            byte[] serial = new byte[response.Length - 5];
+            Array.Copy(response, 4, serial, 0, serial.Length);
+
+            return Encoding.ASCII.GetString(serial).TrimEnd((Char)0);
+        }
+
+        public async Task OpenAsync()
+        {
+            if (!await _hidDevice.OpenDeviceAsync())
+                throw new Exception("Could not open device!");
+            SerialNumber = await ReadSerialNumberAsync();
+            _hidDevice.BeginRead();
+            _active = true;
+            _configureTask = ConfigureLoop();
+        }
+        public void Open()
+        {
+            if (!_hidDevice.OpenDevice())
+                throw new Exception("Could not open device!");
+            SerialNumber = ReadSerialNumber();
+            _hidDevice.BeginRead();
+            _active = true;
+            _configureTask = ConfigureLoop();
+        }
+
+
+    }
+}
